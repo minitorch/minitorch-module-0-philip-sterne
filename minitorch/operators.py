47c91299a52f54a00299718e053ce24d9cac24d1
Philip Sterne <sterne@gmail.com>
2025-02-04 12:05:40 +0200

    Progress on tasks 0.1 - 0.5

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -33,6 +33,71 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    return x * y
+
+
+def id(x: float) -> float:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> bool:
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    return max(0, x)
+
+
+def log(x: float) -> float:
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def log_back(x: float, y: float) -> float:
+    return y / x
+
+
+def inv(x: float) -> float:
+    return 1 / x
+
+
+def inv_back(x: float, y: float) -> float:
+    return -1 / (x**2) * y
+
+
+def relu_back(x: float, y: float) -> float:
+    return y if x > 0 else 0
+
+
+# ## Task 0.2
 
 
 # ## Task 0.3
@@ -52,3 +117,33 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+def map(fn: Callable[[float], float], l: Iterable[float]) -> Iterable[float]:
+    return [fn(x) for x in l]
+
+
+def zipWith(
+    fn: Callable[[float, float], float], l1: Iterable[float], l2: Iterable[float]
+) -> Iterable[float]:
+    return [fn(x, y) for x, y in zip(l1, l2)]
+
+
+def reduce(
+    fn: Callable[[float, float], float], l: Iterable[float], init: float
+) -> float:
+    return init if not l else reduce(fn, l[1:], fn(init, l[0]))
+
+
+def negList(l: Iterable[float]) -> Iterable[float]:
+    return map(neg, l)
+
+
+def addLists(l1: Iterable[float], l2: Iterable[float]) -> Iterable[float]:
+    return zipWith(add, l1, l2)
+
+
+def sum(l: Iterable[float]) -> float:
+    return reduce(add, l, 0)
+
+
+def prod(l: Iterable[float]) -> float:
+    return reduce(mul, l, 1)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -13,6 +13,7 @@ from minitorch.operators import (
     id,
     inv,
     inv_back,
+    log,
     log_back,
     lt,
     max,
@@ -25,7 +26,7 @@ from minitorch.operators import (
     sigmoid,
 )
 
-from .strategies import assert_close, small_floats
+from .strategies import assert_close, small_floats, small_positive_floats
 
 # ## Task 0.1 Basic hypothesis tests.
 
@@ -107,41 +108,53 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
+    assert sigmoid(a) <= sigmoid(a + 1e-3)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a < b and b < c:
+        assert a < c
+    if c < b and b < a:
+        assert c < a
+    if a < c and c < b:
+        assert a < b
+    if c < a and a < b:
+        assert c < b
+    if b < a and a < c:
+        assert b < c
+    if b < c and c < a:
+        assert b < a
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, b), mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x: float, y: float, z: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(z, add(x, y)), add(mul(z, x), mul(z, y)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_positive_floats, small_positive_floats)
+def test_other(a: float, b: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(log(mul(a, b)), add(log(a), log(b)))
 
 
 # ## Task 0.3  - Higher-order functions
@@ -168,8 +181,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    assert_close(sum(ls1) + sum(ls2), sum(addLists(ls1, ls2)))
 
 
 @pytest.mark.task0_3
